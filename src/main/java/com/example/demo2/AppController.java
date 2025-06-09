e9e8f4d2764f1583aacc0be85a34b232bd6486ca
Anhyeonggwan <anhyeonggwan@gmail.com>
2025-06-09 15:45:28 +0900

    cicd settting

---
+++ src/main/java/com/example/demo2/AppController.java
@@ -0,0 +1,27 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class AppController {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping("/redis")
+    public String redisMethod() {
+        redisTemplate.opsForValue().set("abc", "Hello World");
+        return "redis";
+    }
+    
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return "hello world";
+    }
+    
+
+}
